37df280c34d5fddbe30c408abcc15025e985edeb
cesar zubilete <brycezusan@gmail.com>
2024-07-01 13:46:02 -0500

    update footer section  challengue jr

--- 6.carrito-uni-jr/src/layouts/Layout.jsx
+++ 6.carrito-uni-jr/src/layouts/Layout.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/header";
+import { Footer } from "../components/footer";
 
 // eslint-disable-next-line react/prop-types
 export const Layout = () => {
   return (
     <>
       <Header />
-      <main className="min-h-screen xl:min-h-full mt-[200px] xl:mt-[130px] contenedor ">
+      <main className="min-h-screen xl:min-h-full mt-[200px] xl:my-[130px] contenedor ">
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 };
